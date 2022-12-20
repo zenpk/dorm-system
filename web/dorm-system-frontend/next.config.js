a51075afe4ab8d5886a716ad89145b1997c362d5
zenpk <99558412+zenpk@users.noreply.github.com>
2022-12-21 01:38:28 +0800

    fix

--- web/dorm-system-frontend/next.config.js
+++ web/dorm-system-frontend/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 module.exports = {
     ...nextConfig,
     publicRuntimeConfig: {
+        // url: "http://127.0.0.1:8080"
         url: "http://127.0.0.1:8080"
     },
     env: {
