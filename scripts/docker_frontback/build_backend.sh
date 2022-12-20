41520ac12fea2358edf9dfcb0310aacbfe8a73c4
zenpk <99558412+zenpk@users.noreply.github.com>
2022-12-21 01:34:05 +0800

    fix

--- scripts/docker_frontback/build_backend.sh
+++ scripts/docker_frontback/build_backend.sh
@@ -1,2 +1,2 @@
-docker build -t dorm-system-backend .
-docker run -dp 8080:8080 --name dorm-system-backend --add-host=host.docker.internal:host-gateway dorm-system-backend
+sudo docker build -t dorm-system-backend .
+sudo docker run -dp 8080:8080 --name dorm-system-backend --add-host=host.docker.internal:host-gateway dorm-system-backend
